9e175fd154ba5b6798a58144566a180d12ea8882
iTBoyer <iTBoyer@qq.com>
2020-06-04 16:17:21 +0800

    update

--- Others/Jonyfive/Sources/setup.swift
+++ Others/Jonyfive/Sources/setup.swift
@@ -2,8 +2,7 @@ import Guaka
 
 // Generated, dont update
 func setupCommands() {
-  rootCommand.add(subCommand: metaCommand)
-  rootCommand.add(subCommand: webvttCommand)
+    rootCommand.add(subCommand: metaCommand)
+    rootCommand.add(subCommand: webvttCommand)
     rootCommand.add(subCommand: subtitleCommand)
-  // Command adding placeholder, edit this line
 }
