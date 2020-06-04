b566815159b63869476df00c3435001152fcb364
iTBoyer <iTBoyer@qq.com>
2020-06-04 11:09:37 +0800

    集成命令行参数解析库

--- Package.swift
+++ Package.swift
@@ -5,13 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "iSPMCmd",
-//    products: [
-//        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-//        .library(
-//            name: "SPMCmdLineTool",
-//            targets: ["SPMCmdLineTool"]
-//        )
-//    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+//        .library(name: "iSPMCmd", targets: ["iSPMCmd"])
+    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -23,12 +20,16 @@ let package = Package(
         .package(name:"Guaka", url: "https://github.com/oarrabi/Guaka.git", from: "0.0.0"),
         // XML/HTML 解析工具
         .package(name:"Kanna", url: "https://github.com/tid-kijyun/Kanna.git", from: "5.2.2"),
+        // 解析命令行参数
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "iSPMCmd",
-                dependencies: ["FilesProvider", "PerfectPython","Guaka","Kanna"],
+                dependencies: ["FilesProvider", "PerfectPython","Guaka","Kanna",
+                                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                ],
                 path:"Sources",
                 exclude:["DesignPatterns","Panagrams","SwiftWebUI"]
         ),
