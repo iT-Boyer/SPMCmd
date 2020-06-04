a9066c268aae7f3a754c9a7aae6e43e9f177c93a
iTBoyer <iTBoyer@qq.com>
2020-06-04 10:17:11 +0800

    重构iSPMCmd目录结构，符合guaka架构，并执行验证

--- Package.swift
+++ Package.swift
@@ -27,8 +27,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(name: "iSPMCmd",
                 dependencies: ["FilesProvider", "PerfectPython","Guaka","Kanna"],
-                path:"Sources/iSPMCmd",
-                sources:["Sources"]
+                path:"Sources",
+                exclude:["DesignPatterns","Panagrams","SwiftWebUI"]
         ),
         
         .testTarget(name: "SPMCmdLineToolTests",
--- Sources/setup.swift
+++ Sources/setup.swift
@@ -3,5 +3,6 @@ import Guaka
 // Generated, dont update
 func setupCommands() {
   rootCommand.add(subCommand: checkCommand)
+  rootCommand.add(subCommand: checkoutCommand)
   // Command adding placeholder, edit this line
 }
