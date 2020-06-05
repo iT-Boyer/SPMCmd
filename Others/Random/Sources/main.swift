4216491bd48afef92fa2ba82463c5a7a8eb48385
iTBoyer <iTBoyer@qq.com>
2020-06-05 13:41:26 +0800

    添加随机选择元素命令

--- Others/Random/Sources/main.swift
+++ Others/Random/Sources/main.swift
@@ -6,12 +6,12 @@ import ArgumentParser
 struct Random: ParsableCommand {
     // 自定义设置
     static var configuration =
-      CommandConfiguration(commandName: "掷骰子", // 自定义命令名，默认是类型名
+      CommandConfiguration(commandName: "random", // 自定义命令名，默认是类型名
                            abstract: "掷骰子",
                            discussion: "练习swift终端命令编写设计",
                            version: "1.0.0",
                            shouldDisplay: true,
-                           subcommands: [Number.self],
+                           subcommands: [Number.self,Pick.self],
                            defaultSubcommand: Number.self,
                            helpNames: NameSpecification.customLong("h"))
 }
@@ -23,7 +23,7 @@ extension Random {
         static var configuration = CommandConfiguration(abstract:"在1和最大值之间随机选择一个数字",
                                                         shouldDisplay: true)
         //MARK: 参数
-        @Argument(help: "最大值")
+        @Argument(help: "输入随机数中的最大值")
         var highVaule: Int
         //MARK: flag
         
@@ -36,6 +36,34 @@ extension Random {
         func run() throws {
             //MARK: 命令操作
             print(Int.random(in: 1...highVaule))
+        }
+    }
+}
+
+//
+extension Random {
+    //MARK: 定义自命令 struct结构体
+    struct Pick:ParsableCommand {
+        //MARK: 配置
+        static var configuration = CommandConfiguration(abstract:"从给定的数组中，随机抽选其中一个元素",
+                                                        shouldDisplay: true)
+        //MARK: 参数
+        @Option(default:3, help:"随机抽取的元素的个数")
+        var count:Int
+        @Argument(help: "输入待抽取的元素队列")
+        var elements:[String]
+        //MARK: flag
+        
+        //MARK: 校验
+        func validate() throws {
+            guard !elements.isEmpty else {
+                throw ValidationError("元素不存在，请输入再抽取")
+            }
+        }
+        func run() throws {
+            //MARK: 命令操作
+            let picks =  elements.shuffled().prefix(count)
+            print(picks.joined(separator: "\n"))
         }
     }
 }
