74200e8c25fd64e4a9c2efa7c6206453bbab9469
iTBoyer <iTBoyer@qq.com>
2020-06-09 18:54:39 +0800

    Quick和iOS平台在同一个workspace中冲突

--- Package.swift
+++ Package.swift
@@ -26,6 +26,9 @@ let package = Package(
         .package(url: "https://github.com/jpsim/Yams.git", from: "3.0.1"),
         .package(url: "https://github.com/nsomar/FileUtils", from: "0.2.0"),
         .package(url: "https://github.com/sharplet/Regex.git", from: "2.1.1"),
+        //quick行为测试
+        .package(url: "https://github.com/Quick/Quick.git", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "7.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
---
+++ Tests/SPMCmdLineToolTests/teste.swift
@@ -0,0 +1,16 @@
+//
+//  teste.swift
+//  iSPMCmd
+//
+//  Created by jhmac on 2020/6/9.
+//  
+//
+
+import Quick
+import Nimble
+
+class teste: QuickSpec {
+    override func spec() {
+
+    }
+}
