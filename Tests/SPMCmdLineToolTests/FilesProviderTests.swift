8c2e4d161f29c8a96b390ad7527eab51027d40ad
iTBoyer <iTBoyer@qq.com>
2020-07-28 18:10:20 +0800

    替换图片的脚本实现

--- Tests/SPMCmdLineToolTests/FilesProviderTests.swift
+++ Tests/SPMCmdLineToolTests/FilesProviderTests.swift
@@ -12,9 +12,12 @@ class FilesProviderTests: XCTestCase {
 
     var provider:LocalFileProvider!
     let timeout: Double = 60.0
+    var testBundle:Bundle!
+    let sampleText = "Hello world!"
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         provider = LocalFileProvider(for: .userDirectory, in: .allDomainsMask)
+        testBundle = Bundle.init(for: FilesProviderTests.self)
     }
 
     override func tearDown() {
@@ -48,12 +51,60 @@ class FilesProviderTests: XCTestCase {
         }
         print("Test fulfilled: \(desc).")
     }
-
+    
+    func testRenames() {
+        let desc = "Copying file in \(provider.type)"
+        print("Test started: \(desc).")
+        let expectation = XCTestExpectation(description: desc)
+        // 1. 菜单名重命名为icon名
+        //       let plistPath = testBundle.path(forResource: "personalNewPage", ofType: "plist")
+        let plistPath = "/Users/jhmac/hsg/SupervisionSel/SupervisionSelTests/Jinher/personalNew/personalNewPage.plist"
+        let iconDoc = "jhmac/Desktop/icon"
+        let toPath = "jhmac/hsg/hppersonalpagelib/HDPersonalPageBundle/personalImages"
+        let dicInfo = NSDictionary(contentsOfFile: plistPath)
+        let menus:Array = dicInfo?.object(forKey: "menu") as! Array<NSDictionary>
+        for i in 0..<menus.count {
+            let icons:Array = menus[i].object(forKey: "array") as! Array<NSDictionary>
+            for j in 0..<icons.count {
+                let icon:NSDictionary = icons[j]
+                let iconstr = icon.object(forKey: "icon")
+                let iconname = icon.object(forKey: "name")
+                //               print("\(iconname!):\(iconstr!)")
+                let filePath2 = "\(iconDoc)/\(iconname!)@2x.png"
+                let toPath2 = "\(toPath)/\(iconstr!)@2x.png"
+                let filePath3 = "\(iconDoc)/\(iconname!)@3x.png"
+                let toPath3 = "\(toPath)/\(iconstr!)@3x.png"
+                
+                let ext2 = "/Users/\(filePath2)"
+                let ext3 = "/Users/\(filePath3)"
+                
+                if FileManager.default.fileExists(atPath: ext2) {
+                    print("准备替换：\(filePath2)")
+                    provider.removeItem(path: toPath2) { (error) in
+                        self.provider.moveItem(path: filePath2, to: toPath2, completionHandler: nil)
+                    }
+                }
+                if FileManager.default.fileExists(atPath: ext3) {
+                    print("准备替换：\(filePath3)")
+                    provider.removeItem(path: toPath3) { (error) in
+                        self.provider.moveItem(path: filePath3, to: toPath3, overwrite: true) { (error) in
+                            if j == menus.count - 1
+                            {
+                                expectation.fulfill()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        waitForExpectations(timeout: 100, handler: nil)
+    }
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
+    
 }
